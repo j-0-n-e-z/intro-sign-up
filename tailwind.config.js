ea8d8e12b782ee52834ad9862314a607301c127b
j-0-n-e-z <j000neeez@gmail.com>
2023-05-11 14:58:04 +0500

    add mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,12 @@ export default {
 				'grayish-blue': 'hsl(246, 25%, 77%)'
       },
       backgroundImage: {
-        desktop: 'url(/assets/images/bg-intro-desktop.png)'
+        desktop: 'url(/assets/images/bg-intro-desktop.png)',
+        mobile: 'url(/assets/images/bg-intro-mobile.png)'
 			},
 			boxShadow: {
-				grayishBlue: '0 8px 0 0 #3e3c493f'
+				grayishBlue: '0 8px 0 0 #3e3c493f',
+				grayishBlueInset: 'inset 0 -4px 0 0 #3e3c493f',
 			}
 		}
 	},
